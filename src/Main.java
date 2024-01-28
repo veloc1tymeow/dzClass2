b2d39b4f453bc91a4df532e561695e3651e2ec81
veloc1ty <odexta02@gmail.com>
2024-01-28 12:34:09 +0300

    Доавил неактивного активность участников

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,38 @@ class Participant {
     protected String name;
     protected int maxRunDistance;
     protected int maxJumpHeight;
+    protected boolean active; // Добавлен флаг для отслеживания активности участника
 
     public Participant(String name, int maxRunDistance, int maxJumpHeight) {
         this.name = name;
         this.maxRunDistance = maxRunDistance;
         this.maxJumpHeight = maxJumpHeight;
+        this.active = true; // Изначально участник активен
     }
 
     public void run(int distance) {
-        if (distance <= maxRunDistance) {
-            System.out.println(name + " успешно пробежал " + distance + " метров");
+        if (active) {
+            if (distance <= maxRunDistance) {
+                System.out.println(name + " успешно пробежал " + distance + " метров");
+            } else {
+                System.out.println(name + " не смог пробежать " + distance + " метров и выбывает");
+                active = false; // Если не смог пробежать, делаем участника неактивным
+            }
         } else {
-            System.out.println(name + " не смог пробежать " + distance + " метров");
+            System.out.println(name + " уже выбыл");
         }
     }
 
     public void jump(int height) {
-        if (height <= maxJumpHeight) {
-            System.out.println(name + " успешно прыгнул на высоту " + height + " метра");
+        if (active) {
+            if (height <= maxJumpHeight) {
+                System.out.println(name + " успешно прыгнул на высоту " + height + " метра");
+            } else {
+                System.out.println(name + " не смог прыгнуть на высоту " + height + " метра и выбывает");
+                active = false; // Если не смог прыгнуть, делаем участника неактивным
+            }
         } else {
-            System.out.println(name + " не смог прыгнуть на высоту " + height + " метра");
+            System.out.println(name + " уже выбыл");
         }
     }
 }
@@ -87,10 +99,14 @@ public class Main {
 
         for (Participant participant : participants) {
             for (Object obstacle : obstacles) {
-                if (obstacle instanceof Track) {
-                    ((Track) obstacle).pass(participant);
-                } else if (obstacle instanceof Wall) {
-                    ((Wall) obstacle).pass(participant);
+                if (participant.active) {
+                    if (obstacle instanceof Track) {
+                        ((Track) obstacle).pass(participant);
+                    } else if (obstacle instanceof Wall) {
+                        ((Wall) obstacle).pass(participant);
+                    }
+                } else {
+                    break; // Если участник уже неактивен, прерываем прохождение препятствий
                 }
             }
             System.out.println();
